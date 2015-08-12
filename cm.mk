826cfe614f2f9265f4852e961b1b06ba2ca9f682
halaszk <halaszk@deathlord.eu>
2015-08-12 09:47:30 +0200

    initial cm12.1 support

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 # Release name
 PRODUCT_RELEASE_NAME := treltexx
 
-# Inherit some common EOS stuff.
-$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit nfc enhanced configuration
-#$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/treltexx/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := eos_treltexx
+PRODUCT_NAME := cm_treltexx
 PRODUCT_DEVICE := treltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N910C
